9c957c94316ff2a48840e1552a7d0a1a08a6b4ed
ThinMintz <rachel.rodriguez05@gmail.com>
2017-12-18 21:50:31 -0600

    ran database

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ("New Bacon-ings");
+INSERT INTO burgers (burger_name) VALUES ("Rest in Peas Burger");
+INSERT INTO burgers (burger_name) VALUES ("Hit Me With Your Best Shallot Burger");
+INSERT INTO burgers (burger_name) VALUES ("I've Created a Muenster Burger");
